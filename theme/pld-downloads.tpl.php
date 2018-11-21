45db758047be7b8b9e071d803c00b62332af96bf
Digital Content Coordinator <dcc@Digitals-iMac.local>
2018-11-21 15:39:07 -0500

    initial commit of module

---
+++ theme/pld-downloads.tpl.php
@@ -0,0 +1,27 @@
+<?php
+/**
+*	Displays markup for available datastream downloads
+**/
+
+var_dump($variables['items']);
+
+?>
+
+<div class="pld-downloads__wrapper">
+	
+	<h3>In Collections: 
+		<span class="collection-label"></span>
+	</h3>
+	
+	<div class="finding-aid__wrapper">
+		<h4>Collection Finding Aid</h4>
+	</div>
+	
+	<div class="datastreams__wrapper">
+		<h4>Datastreams</h4>
+		<div class="islandora-basic-image-download-btns__wrapper">
+
+        </div>
+      </div>
+
+</div>
